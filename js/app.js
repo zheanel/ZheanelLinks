4b986818f1c1fe1f41b6df55daf44c5c9501ff1b
skoopeyt <53342294+zheanel@users.noreply.github.com>
2025-01-28 13:49:12 +0100

    AutoDate

--- js/app.js
+++ js/app.js
@@ -29,6 +29,11 @@ $(document).ready(function () {
   });
 });
 
+const birthYear = 2003;
+const currentYear = new Date().getFullYear();
+const age = currentYear - birthYear;
+
+document.getElementById("age").textContent = age;
 
 function birthday() {
   // Birthday Reminder!
